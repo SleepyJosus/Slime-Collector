84ca453bb0dd8007a11809f4bb65b9d0c0c56b4d
Joe Gerhard <jlgerhard@gmail.com>
2019-09-10 13:54:06 -0500

    add class views, add full CRUD to Slime model

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('slimes/', views.slimes_index, name='index'),
-    path('slimes/<int:slime_id>/', views.slimes_detail, name="detail")
+    path('slimes/<int:slime_id>/', views.slimes_detail, name="detail"),
+    path('slimes/create/', views.SlimeCreate.as_view(), name='slimes_create'),
+    path('slimes/<int:pk>/update/', views.SlimeUpdate.as_view(), name="slimes_update"),
+    path('slimes/<int:pk>/delete/', views.SlimeDelete.as_view(), name="slimes_delete"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Slime
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello World')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -18,3 +17,15 @@ def slimes_index(request):
 def slimes_detail(request, slime_id):
     slime = Slime.objects.get(id=slime_id)
     return render(request, 'slimes/detail.html', {'slime': slime})
+
+class SlimeCreate(CreateView):
+    model = Slime
+    fields = '__all__'
+
+class SlimeUpdate(UpdateView):
+    model = Slime
+    fields = ['breed', 'description', 'age']
+
+class SlimeDelete(DeleteView):
+    model = Slime
+    success_url = '/slimes/'
