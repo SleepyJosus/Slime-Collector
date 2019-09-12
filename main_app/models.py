c05ce208ae012715349b512be259fb9ebc9c9b10
Joe Gerhard <jlgerhard@gmail.com>
2019-09-11 20:30:57 -0500

    Finish many-to-many section of lab

--- main_app/models.py
+++ main_app/models.py
@@ -1,17 +1,28 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("toy_detail", kwargs={"pk": self.pk})
 
 class Slime(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse("detail", kwargs={"slime_id": self.id})
+    
+
     
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Slime
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from .models import Slime, Toy
 
 # Create your views here.
 
@@ -16,7 +18,11 @@ def slimes_index(request):
 
 def slimes_detail(request, slime_id):
     slime = Slime.objects.get(id=slime_id)
-    return render(request, 'slimes/detail.html', {'slime': slime})
+    toys_slime_doesnt_have = Toy.objects.exclude(id__in = slime.toys.all().values_list('id'))
+    return render(request, 'slimes/detail.html', {
+        'slime': slime,
+        'toys': toys_slime_doesnt_have,
+        })
 
 class SlimeCreate(CreateView):
     model = Slime
@@ -25,7 +31,29 @@ class SlimeCreate(CreateView):
 class SlimeUpdate(UpdateView):
     model = Slime
     fields = ['breed', 'description', 'age']
-
+    
 class SlimeDelete(DeleteView):
     model = Slime
     success_url = '/slimes/'
+
+class ToyListView(ListView):
+    queryset = Toy.objects.all()
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyDetailView(DetailView):
+    queryset = Toy.objects.all()
+
+class ToyUpdateView(UpdateView):
+    model = Toy
+    fields = ['color']
+
+class ToyDeleteView(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+def assoc_toy(request, slime_id, toy_id):
+    Slime.objects.get(id=slime_id).toys.add(toy_id)
+    return redirect('detail', slime_id=slime_id)
